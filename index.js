bf1542baedf1fc0455600526ff9c4df2d0a15689
Iman Jawad <imanj12@gmail.com>
2018-06-27 19:39:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,20 @@ function subtract(a, b) {
   return a - b
 }
 
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a,b) {
+  return a / b
+}
+
+function inc(n) {
+  return n++
+}
+
+function dec(n) {
+  return n--
+}
+
 function 
